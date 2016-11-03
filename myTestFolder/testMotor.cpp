94d744dde2fb8a17da455e45ba176d7e119554cf
Robert Raspi <robert.jonas@gmx.at>
2016-11-03 21:56:15 +0100

    Stepmotor: Ausgabe

--- myTestFolder/testMotor.cpp
+++ myTestFolder/testMotor.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 #include <wiringPi.h>
 int main (void)
 {
@@ -6,7 +8,7 @@ int main (void)
   // Pin 0 bis 8 auf Output setzen
   for (int i=0;i<8;++i)
   {
-	int pin=i; 
+    int pin=i; 
     pinMode (pin, OUTPUT);
   }
   
@@ -14,7 +16,8 @@ int main (void)
   // Zu Testzwecken lass LEDs am MotorShield blinken
   for (int i=0;i<8;++i)
   {
-	int pin=i;  
+    int pin=i;
+    cout << "Pin: " << i << endl;
     digitalWrite (pin, HIGH) ; delay (500);
     digitalWrite (pin,  LOW) ; delay (500);
   }
