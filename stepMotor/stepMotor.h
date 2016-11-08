424b493e384b3e194dbde1a0968b83790bad96d9
Robert Master PC <robert.jonas@gmx.at>
2016-11-08 17:50:49 +0100

    class stepMotor started

---
+++ stepMotor/stepMotor.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+using namespace std;
+/*
+	stepMotor class declarition for Raspberry stepper motors via GPIO pins
+	using wiringPI (http://wiringpi.com/) moduls
+*/
+
+//================================================================================================================================================
+class stepMotor {
+public:
+	stepMotor(int pinA=0,int pinB=1,int pinC=2,int pinD=3,int stepDelay=20);
+	void stepForward(int steps);
+	void stepBackward(int steps);
+	void rotate(int rounds=1);
+private:
+	int pinA,pinB,pinC,pinD;
+	int stepDelay;
+
+};
